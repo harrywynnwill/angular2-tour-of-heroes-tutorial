3780130e43f6be0cac761df0fa87a5f1241fc31e
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-10-05 14:53:06 +0100

    services

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
+#Angular 2 Walkthrough
+
+###NOTES
+
 Components are the basic building blocks of Angular applications. A component controls a portion of the screen—a view—through its associated template
 
 
 `<input [(ngModel)]="hero.name" placeholder="name">`
+
+`<li *ngFor="let hero of heroes">`
+
+The `*` prefix to `ngFor` indicates that the <li> element and its children constitute a master template.
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component'
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
   bootstrap: [ AppComponent ]
 })
-
 export class AppModule { }
